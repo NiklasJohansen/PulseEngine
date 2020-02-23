70792d42dda79a8421e9d4fe6f779506a49318c0
Niklas Johansen <nikkojohansen@hotmail.com>
2020-02-23 20:06:53 +0100

    Fix fullscreen on different monitors

--- src/main/kotlin/engine/modules/Window.kt
+++ src/main/kotlin/engine/modules/Window.kt
@@ -63,15 +63,14 @@ class Window : WindowEngineInterface
         this.width = initWidth
         this.height = initHeight
 
-        val monitor = if(screenMode == FULLSCREEN)
+        var monitor = NULL
+        if (screenMode == FULLSCREEN)
         {
-            val primaryMonitor = glfwGetPrimaryMonitor()
-            val videoMode = glfwGetVideoMode(primaryMonitor)
+            monitor = getWindowMonitor()
+            val videoMode = glfwGetVideoMode(monitor)
             width = videoMode?.width() ?: width
             height = videoMode?.height() ?: height
-            primaryMonitor
         }
-        else NULL
 
         val newWindowHandle = glfwCreateWindow(width, height, title, monitor, windowHandle)
         if (newWindowHandle == NULL)
@@ -81,13 +80,15 @@ class Window : WindowEngineInterface
         if(windowHandle != NULL)
             glfwDestroyWindow(windowHandle)
 
+        this.windowHandle = newWindowHandle
+
         if(screenMode == WINDOWED)
         {
-            val mode = glfwGetVideoMode(glfwGetPrimaryMonitor())!!
-            glfwSetWindowPos(newWindowHandle, (mode.width() - width) / 2, (mode.height() - height) / 2)
+            val mode = glfwGetVideoMode(getWindowMonitor())!!
+            glfwSetWindowPos(windowHandle, (mode.width() - width) / 2, (mode.height() - height) / 2)
         }
 
-        glfwSetWindowSizeCallback(newWindowHandle, object : GLFWWindowSizeCallback()
+        glfwSetWindowSizeCallback(windowHandle, object : GLFWWindowSizeCallback()
         {
             override fun invoke(window: Long, width: Int, height: Int)
             {
@@ -97,10 +98,9 @@ class Window : WindowEngineInterface
             }
         })
 
-        glfwMakeContextCurrent(newWindowHandle)
+        glfwMakeContextCurrent(windowHandle)
         glfwSwapInterval(0)
-        glfwShowWindow(newWindowHandle)
-        this.windowHandle = newWindowHandle
+        glfwShowWindow(windowHandle)
     }
 
     override fun updateScreenMode(mode: ScreenMode)
@@ -128,4 +128,39 @@ class Window : WindowEngineInterface
         glfwDestroyWindow(windowHandle)
         glfwTerminate()
     }
+
+    private fun getWindowMonitor(): Long
+    {
+        if(windowHandle == NULL)
+            return glfwGetPrimaryMonitor()
+
+        val xWindow = IntArray(1)
+        val yWindow = IntArray(1)
+        val widthWindow = IntArray(1)
+        val heightWindow = IntArray(1)
+        glfwGetWindowPos(windowHandle, xWindow, yWindow)
+        glfwGetWindowSize(windowHandle, widthWindow, heightWindow)
+
+        val xWindowCenter = xWindow[0] + widthWindow[0] / 2
+        val yWindowCenter = yWindow[0] + heightWindow[0] / 2
+
+        return getMonitors().firstOrNull { monitor ->
+            val videoMode = glfwGetVideoMode(monitor)
+            val widthMonitor = videoMode?.width() ?: 0
+            val heightMonitor = videoMode?.height() ?: 0
+            val xMonitor = IntArray(1)
+            val yMonitor = IntArray(1)
+            glfwGetMonitorPos(monitor, xMonitor, yMonitor)
+
+            // True if center of window is within the monitor bounds
+            xWindowCenter >= xMonitor[0] && xWindowCenter <= xMonitor[0] + widthMonitor &&
+            yWindowCenter >= yMonitor[0] && yWindow[0] <= yWindowCenter + heightMonitor
+
+        } ?: glfwGetPrimaryMonitor()
+    }
+
+    private fun getMonitors(): List<Long> =
+        glfwGetMonitors()?.let {
+            monitors -> 0.until(monitors.limit()).mapNotNull { monitors[it] }
+        } ?: emptyList()
 }
