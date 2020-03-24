9f0731783fff110d6b5869d15d2424bc23c70f82
Niklas Johansen <nikkojohansen@hotmail.com>
2020-03-24 21:22:13 +0100

    Use graphics implementation in text renderer

--- src/main/kotlin/engine/modules/graphics/TextRenderer.kt
+++ src/main/kotlin/engine/modules/graphics/TextRenderer.kt
@@ -20,15 +20,15 @@ class TextRenderer
     private val advanceWidth = IntArray(1)
     private val leftSideBearing = IntArray(1)
 
-    fun draw(text: String,  x: Float, y: Float, font: Font, fontSize: Float, rotation: Float, xOrigin: Float, yOrigin: Float)
+    fun draw(gfx: GraphicsEngineInterface, text: String, x: Float, y: Float, font: Font, fontSize: Float, xOrigin: Float, yOrigin: Float)
     {
         val fontIndex = if(fontSize != -1f) font.fontSizes.indexOf(fontSize) else 0
         if(fontIndex == -1)
             throw IllegalArgumentException("Font size $fontSize not among available sizes [${font.fontSizes.joinToString()}] in font asset: ${font.name}")
 
-        val charData = font.characterData
-        val width = font.characterImage.width
-        val height = font.characterImage.height
+        val charData = font.charData
+        val width = font.charTexture.width
+        val height = font.charTexture.height
         val fontHeight = font.fontSizes[fontIndex]
         val scale = stbtt_ScaleForPixelHeight(font.info, fontHeight)
 
@@ -44,40 +44,17 @@ class TextRenderer
         yb[0] = y + yOffset
         charData.position(fontIndex * 3 * 128)
 
-        GL11.glPushMatrix()
-        glEnable(GL_TEXTURE_2D)
-        glBindTexture(GL_TEXTURE_2D, font.characterImage.textureId)
-
-        if (rotation != 0f)
-        {
-            glTranslatef(x, y, 0f)
-            glRotatef(rotation, 0f, 0f, 1f)
-            glTranslatef(-x, -y, 0f)
-        }
-
-        glBegin(GL_QUADS)
         for (character in text)
         {
             stbtt_GetPackedQuad(charData, width, height, character.toInt(), xb, yb, quad, true)
-            drawQuad(
-                quad.x0(), quad.y0(), quad.x1(), quad.y1(),
-                quad.s0(), quad.t0(), quad.s1(), quad.t1()
-            )
-        }
-        glEnd()
-        GL11.glPopMatrix()
-    }
 
-    private fun drawQuad(x0: Float, y0: Float, x1: Float, y1: Float, s0: Float, t0: Float, s1: Float, t1: Float)
-    {
-        glTexCoord2f(s0, t0)
-        glVertex2f(x0, y0)
-        glTexCoord2f(s1, t0)
-        glVertex2f(x1, y0)
-        glTexCoord2f(s1, t1)
-        glVertex2f(x1, y1)
-        glTexCoord2f(s0, t1)
-        glVertex2f(x0, y1)
+            val xChar = quad.x0()
+            val yChar = quad.y0()
+            val charWidth = quad.x1() - xChar
+            val charHeight = quad.y1() - yChar
+
+            gfx.drawImage(font.charTexture, xChar, yChar, charWidth, charHeight, uMin = quad.s0(), vMin = quad.t0(), uMax = quad.s1(), vMax = quad.t1())
+        }
     }
 
     private fun getWidth(text: String, fontInfo: STBTTFontinfo, scale: Float): Float
