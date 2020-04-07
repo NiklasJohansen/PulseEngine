94ab8ac9ec65cd64249e1ccd72c5bdf6bbd0df5e
Niklas Johansen <nikkojohansen@hotmail.com>
2020-04-08 00:35:53 +0200

    Refactor graphics state variables into its own class

---
+++ src/main/kotlin/engine/modules/graphics/GraphicsState.kt
@@ -0,0 +1,42 @@
+package engine.modules.graphics
+
+import org.joml.Matrix4f
+
+class GraphicsState
+{
+    lateinit var projectionMatrix: Matrix4f
+    lateinit var modelMatrix: Matrix4f
+
+    val textureArray = TextureArray(1024, 1024, 100)
+    var blendFunc = BlendFunction.NORMAL
+    var bgRed = 0.1f
+    var bgGreen = 0.1f
+    var bgBlue = 0.1f
+    var rgba = 0f
+    var depth: Float = 0f
+
+    init
+    {
+        setRGBA(0.1f, 0.1f, 0.1f, 1f)
+    }
+
+    fun increaseDepth()
+    {
+        depth += DEPTH_INC
+    }
+
+    fun setRGBA(r: Float, g: Float, b: Float, a: Float)
+    {
+        val red   = (r * 255).toInt()
+        val green = (g * 255).toInt()
+        val blue  = (b * 255).toInt()
+        val alpha = (a * 255).toInt()
+
+        this.rgba = java.lang.Float.intBitsToFloat((red shl 24) or (green shl 16) or (blue shl 8) or alpha)
+    }
+
+    companion object
+    {
+        private const val DEPTH_INC = 0.0000001f
+    }
+}
