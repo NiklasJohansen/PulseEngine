ca1bb177a9e410b7c3c3f96f6445835c51cf5ef3
Niklas Johansen <nikkojohansen@hotmail.com>
2020-04-26 03:04:52 +0200

    Use console module in ConsoleGUI app

--- src/main/kotlin/engine/apps/ConsoleGUI.kt
+++ src/main/kotlin/engine/apps/ConsoleGUI.kt
@@ -3,6 +3,7 @@ package engine.apps
 import engine.GameEngine
 import engine.data.Font
 import engine.data.Key
+import engine.modules.MessageType
 import java.lang.Integer.max
 import java.lang.Math.min
 
@@ -11,7 +12,7 @@ class ConsoleGUI : EngineApp
     private var active: Boolean = false
     private var textBoxContent = StringBuilder()
     private var cursorPos = 0
-    private val commandLog = mutableListOf<String>()
+    private val commandLog = mutableListOf<ConsoleEntry>()
 
     override fun init(engine: GameEngine)
     {
@@ -60,7 +61,16 @@ class ConsoleGUI : EngineApp
         // Run command
         if (engine.input.wasClicked(Key.ENTER))
         {
-            commandLog.add(textBoxContent.toString())
+            val commandString = textBoxContent.toString()
+            val result = engine.console.run(commandString)
+
+            commandLog.add(ConsoleEntry("> $commandString", MessageType.INFO))
+            if (result.message.isNotBlank())
+            {
+                commandLog.add(ConsoleEntry(result.message, result.type))
+                commandLog.add(ConsoleEntry("", MessageType.INFO))
+            }
+
             textBoxContent.clear()
             cursorPos = 0
         }
@@ -77,7 +87,7 @@ class ConsoleGUI : EngineApp
         val availableWidth = width - TEXT_PADDING_X - INPUT_BOX_PADDING
         val charsPerLine = getNumberOfChars(availableWidth)
         val cursorCar = if (System.currentTimeMillis() % 1000 > 500) "|" else " "
-        var text = "> " + StringBuilder(textBoxContent).insert(cursorPos, cursorCar).toString()
+        var text = StringBuilder(textBoxContent).insert(cursorPos, cursorCar).toString()
         val textWidth = getTextWidth(text.length)
         if (textWidth > availableWidth)
             text = text.substring(text.length - charsPerLine)
@@ -93,8 +103,11 @@ class ConsoleGUI : EngineApp
         engine.gfx.drawText(text, TEXT_PADDING_X, height - INPUT_BOX_HEIGHT / 2 + INPUT_BOX_PADDING / 2, cliFont)
 
         var yPos = height - INPUT_BOX_HEIGHT + FONT_SIZE / 2
-        commandLog.reversed().forEach { commandText ->
-            val lines = breakIntoLines("> $commandText", availableWidth)
+        commandLog.reversed().forEach { commandEntry ->
+            val color = MessageColor.from(commandEntry.type)
+            engine.gfx.setColor(color.red, color.green, color.blue)
+
+            val lines = breakIntoLines(commandEntry.message, availableWidth)
             yPos -= lines.size * FONT_SIZE
             lines.forEachIndexed { i, line -> engine.gfx.drawText(line, TEXT_PADDING_X, yPos + i * FONT_SIZE, cliFont) }
         }
@@ -108,11 +121,15 @@ class ConsoleGUI : EngineApp
     private fun getNumberOfChars(availableWidth: Float): Int
         = (availableWidth / (FONT_SIZE / 2f)).toInt()
     
-    private fun breakIntoLines(line: String, availableWidth: Float): List<String>
+    private fun breakIntoLines(textLine: String, availableWidth: Float): List<String>
     {
-        val charsPerLine = getNumberOfChars(availableWidth)
-        val nLines = (getTextWidth(line.length) / availableWidth).toInt() + 1
-        return 0.until(nLines).map { line.substring(it * charsPerLine, min((it + 1) * charsPerLine, line.length)) }
+        return textLine
+            .split("\n")
+            .flatMap { line ->
+                val charsPerLine = getNumberOfChars(availableWidth)
+                val nLines = (getTextWidth(line.length) / availableWidth).toInt() + 1
+                0.until(nLines).map { line.substring(it * charsPerLine, min((it + 1) * charsPerLine, line.length)) }
+            }
     }
 
     override fun cleanup(engine: GameEngine) {}
@@ -124,5 +141,28 @@ class ConsoleGUI : EngineApp
         private const val INPUT_BOX_PADDING = 7f
         private const val INPUT_BOX_HEIGHT = 40f
     }
+}
 
+data class ConsoleEntry(
+    val message: String,
+    val type: MessageType
+)
+
+data class MessageColor(
+    val red: Float,
+    val green: Float,
+    val blue: Float
+) {
+    companion object
+    {
+        private val RED = MessageColor(1f, 0.2f, 0.2f)
+        private val WHITE = MessageColor(1f, 1f, 1f)
+        private val YELLOW = MessageColor(1f, 1f, 0.2f)
+        fun from(type: MessageType) = when(type)
+        {
+            MessageType.INFO -> WHITE
+            MessageType.WARN -> YELLOW
+            MessageType.ERROR -> RED
+        }
+    }
 }
