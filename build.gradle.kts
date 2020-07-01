bae7440aec8081fbf4357568a1a2730e0f757b5f
Niklas Johansen <nikkojohansen@hotmail.com>
2020-07-01 21:20:09 +0200

    Update root project name

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,15 @@ val lwjglVersion = "3.2.3"
 val kotlinVersion = "1.3.72"
 
 var lwjglNatives = when (OperatingSystem.current()) {
+    OperatingSystem.WINDOWS -> if (System.getProperty("os.arch").contains("64")) "natives-windows" else "natives-windows-x86"
+    OperatingSystem.MAC_OS  -> "natives-macos"
     OperatingSystem.LINUX   -> System.getProperty("os.arch").let {
         if (it.startsWith("arm") || it.startsWith("aarch64"))
             "natives-linux-${if (it.contains("64") || it.startsWith("armv8")) "arm64" else "arm32"}"
         else
             "natives-linux"
     }
-    OperatingSystem.MAC_OS  -> "natives-macos"
-    OperatingSystem.WINDOWS -> if (System.getProperty("os.arch").contains("64")) "natives-windows" else "natives-windows-x86"
-    else -> throw Error("Unrecognized or unsupported Operating system. Please set \"lwjglNatives\" manually")
+    else -> throw Error("Unrecognized operating system")
 }
 
 println("OS: $lwjglNatives")
